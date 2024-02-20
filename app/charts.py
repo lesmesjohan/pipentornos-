31b5f6bc336943881eb70406d27089488a5fdd22
johan lesmes <johancoquera321@gmail.com>
2024-02-19 21:51:04 -0500

    se corrigio app

--- app/charts.py
+++ app/charts.py
@@ -1,17 +1,15 @@
 import matplotlib.pyplot as plt
 
-def generate_bar_chart(name, labels, values):
+def generate_bar_chart(labels, values):
   fig, ax = plt.subplots()
   ax.bar(labels, values)
-  plt.savefig(f'./imgs/{name}.png')
-  plt.close()
+  plt.show()
 
 def generate_pie_chart(labels, values):
   fig, ax = plt.subplots()
   ax.pie(values, labels=labels)
   ax.axis('equal')
-  plt.savefig('chart_pie.png')
-  plt.close()
+  plt.show()
 
 if __name__ == '__main__':
   labels = ['a', 'b', 'c']
